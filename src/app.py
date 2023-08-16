b9b46d26443f5e75c1ffb3ddd4a4d796fc936888
Andrea Betancourt <111385731+AndreaBetan@users.noreply.github.com>
2023-08-16 20:37:58 +0000

    V.1.0

--- src/app.py
+++ src/app.py
@@ -26,17 +26,49 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
+def get_members():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+
+    try: 
+        if not members:
+            return jsonify({"message": 'Bad request'}), 404
+        else:
+            return jsonify(members), 200
+    except Exception as e:
+        return jsonify({"message": 'Server error'}), 500
 
 
-    return jsonify(response_body), 200
+@app.route('/members/<int:id>', methods=['GET'])
+def get_member_id(id):
+    member = jackson_family.get_member(id)
+
+    try: 
+        if not member:
+            return jsonify({"message": 'Bad request'}), 404
+        else:
+            return jsonify(member), 200
+    except Exception as e:
+        return jsonify({"message": 'Server error'}), 500
+
+
+@app.route('/delete/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    
+    member = jackson_family.delete_member(id)
+
+    try: 
+        if not member:
+            return jsonify({"message": 'Bad request'}), 404
+        else:
+            return jsonify("Se elimina member id: ", member), 200
+    except Exception as e:
+        return jsonify({"message": 'Server error'}), 500
+
+
+# DELETE: Hacer lo mismo pero trayendo el metodo eliminar
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -9,28 +9,71 @@ update this file to implement the following already declared methods:
 from random import randint
 
 class FamilyStructure:
+    # Estas funciones son metodos
     def __init__(self, last_name):
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [
+            { 
+            "id": self._generateId(),
+            "first_name": "John",
+            "last_name": self.last_name,
+            "age": 33,
+            "lucky_numbers": [7, 13, 22]
+            },
+            { 
+            "id": self._generateId(),
+            "first_name": "Jane",
+            "last_name": self.last_name,
+            "age": 35,
+            "lucky_numbers": [10, 14, 3]
+            },
+            { 
+            "id": self._generateId(),
+            "first_name": "Jimmy",
+            "last_name": self.last_name,
+            "age": 5,
+            "lucky_numbers": [1]
+            }          
+        ]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
         return randint(0, 99999999)
 
-    def add_member(self, member):
+    def add_member(self, id):
         # fill this method and update the return
-        pass
+        member["id"] = self._generateId()
+        self._members.append(member)
+        return self._members
 
     def delete_member(self, id):
         # fill this method and update the return
-        pass
+        members =[]
+
+        for member in self._members:
+            if member["id"] == id:
+                members.remove(member[id])
+
+                self._members = members
+                return self._members
 
     def get_member(self, id):
         # fill this method and update the return
-        pass
+
+        member = []
+        
+        #Recorrer la lista con un for y en cada iteracion ? si id == member_id(Es el que se recibe en el parametro)
+        for member in self._members:
+            if member["id"] == id:
+                member.append(member)
+        
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
+
+jackson_family = FamilyStructure('Jackson')
+# para agregar miembros:
+    # jackson_family.add_member({Definir el objeto})
